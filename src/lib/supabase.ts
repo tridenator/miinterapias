30d81008c05ad70bf95706acbc50d9fe1df414a2
tridenator <platitapami@gmail.com>
2025-08-18 00:19:55 -0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,3 +2,4 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
+if (typeof window !== 'undefined') (window as any).supabase = supabase;
