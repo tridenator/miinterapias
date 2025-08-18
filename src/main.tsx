38170a61298fea54033b26c22291c28c3eaffcc4
tridenator <platitapami@gmail.com>
2025-08-18 00:24:40 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import PublicBooking from './pages/PublicBooking';
 import TherapistPanel from './pages/TherapistPanel';
 import RequireTherapist from './routes/RequireTherapist';
 import RequireAdmin from './routes/RequireAdmin';
 import AdminPanel from './pages/AdminPanel';
+
 import './index.css';
-const router = createBrowserRouter([
-  { path: '/', element: <PublicBooking /> },
-  {
-    path: '/panel',
-    element: (
-      <RequireTherapist>
-        <TherapistPanel />
-      </RequireTherapist>
-    ),
-  },
-  {
-    path: '/panel/admin',
-    element: (
-      <RequireAdmin>
-        <AdminPanel />
-      </RequireAdmin>
-    ),
-  },
-]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PublicBooking />} />
+        <Route
+          path="/panel"
+          element={
+            <RequireTherapist>
+              <TherapistPanel />
+            </RequireTherapist>
+          }
+        />
+        <Route
+          path="/panel/admin"
+          element={
+            <RequireAdmin>
+              <AdminPanel />
+            </RequireAdmin>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
